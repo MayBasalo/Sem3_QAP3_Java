9d3ea87e4b543cbc007173a8a8c11e2a8f23b35c
MayBasalo <May.Basalo@keyin.com>
2025-03-15 18:21:33 -0230

    Updated Demo.java

--- Demo.java
+++ Demo.java
@@ -14,6 +14,22 @@ public class Demo {
         // Displaying updated details
         System.out.println("\nUpdated Person Details:");
         System.out.println(person1);
+
+         // Displaying updated details
+        System.out.println("\nUpdated Person Details:");
+        System.out.println(person1);
+        // Creating a Student object
+        Student lynne = new Student("Lynne Brooke", 16, "F", "HS95129", 3.5);
+        System.out.println(lynne);
+
+         // Creating a Teacher object
+        Teacher mrJava = new Teacher("Duke Java", 34, "M", "Computer Science", 50000);
+        System.out.println(mrJava);
+
+        // Creating a CollegeStudent object
+        CollegeStudent ima = new CollegeStudent("Ima Frosh", 18, "F", "UCB123", 4.0, 1, "English");
+        System.out.println(ima);
+
     }
 }
 
