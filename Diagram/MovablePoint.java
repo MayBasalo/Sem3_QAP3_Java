1ef8c23025d80184889c613aeb2e3ce045774adc
MayBasalo <May.Basalo@keyin.com>
2025-03-17 01:44:43 -0230

    Add files via upload

---
+++ Diagram/MovablePoint.java
@@ -0,0 +1,57 @@
+// MovablePoint.java
+public class MovablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MovablePoint() {
+        super();
+        this.xSpeed = 0.0f;
+        this.ySpeed = 0.0f;
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        super();
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    // New method to set both xSpeed and ySpeed together
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public void move() {
+        setX(getX() + xSpeed);
+        setY(getY() + ySpeed);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Speed=(" + xSpeed + ", " + ySpeed + ")";
+    }
+}
+
+
